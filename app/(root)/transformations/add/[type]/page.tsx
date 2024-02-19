7a29b87fb3170eaf9c036b39c85ce23c7ab3bf44
Yatharth2609 <yatharth.mishra2002@gmail.com>
2024-02-20 01:44:13 +0530

    Form Creation

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import React, { use } from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationsTypePage = () => {
+const AddTransformationsTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
   return (
-    <div>
-      Page
-    </div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default AddTransformationsTypePage
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
+};
+
+export default AddTransformationsTypePage;
