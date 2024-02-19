53ded188a40fdf447807346f74f4ffe0ae245023
Yatharth2609 <yatharth.mishra2002@gmail.com>
2024-02-19 23:07:27 +0530

    first commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationPages = () => {
+  return (
+    <div>
+      TrasformationsPage
+    </div>
+  )
+}
+
+export default TransformationPages
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationsTypePage = () => {
+  return (
+    <div>
+      Page
+    </div>
+  )
+}
+
+export default AddTransformationsTypePage
