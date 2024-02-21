6535ba4cb87dc00ab8db5b2ff8ddab1e27fe8366
Yatharth2609 <yatharth.mishra2002@gmail.com>
2024-02-21 11:38:41 +0530

    Home and Pagination

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -78,7 +78,7 @@ const Sidebar = () => {
                 );
               })}
 
-              <li className="flex-center cursor-pointer gap-2 p-4">
+              <li className="flex-left cursor-pointer gap-2 p-4">
                 <UserButton afterSignOutUrl="/" showName />
               </li>
             </ul>
